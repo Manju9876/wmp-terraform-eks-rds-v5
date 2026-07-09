de90e228f67706f017fe1b41b5792964eedebda4
Manju9876 <manjunathb960650@gmail.com>
2026-07-09 13:27:12 +0530

    github actions practice

---
+++ modules/eks/helm.tf
@@ -0,0 +1,34 @@
+resource "null_resource" "kube-config" {
+
+  depends_on = [aws_eks_node_group.main]
+
+  triggers = {
+    alltime = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name wmp-${var.env}"
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "argocd" {
+
+  depends_on = [null_resource.kube-config]
+
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+
+  set = [
+    {
+      name  = "server.service.type"
+      value = "LoadBalancer"
+    }
+  ]
+}
--- modules/helm/main.tf
+++ modules/helm/main.tf
@@ -1,28 +1,29 @@
-resource "null_resource" "kube_config" {
+# resource "null_resource" "kube_config" {
+#
+#   triggers = {}
+#   provisioner "local-exec" {
+#     command = "aws eks update-kubeconfig --name wmp-${var.env}"
+#   }
+# }
 
-  provisioner "local-exec" {
-    command = "aws eks update-kubeconfig --name wmp-${var.env}"
-  }
-}
+# provider "helm" {
+#   kubernetes = {
+#     config_path = "~/.kube/config"
+#   }
+# }
 
-provider "helm" {
-  kubernetes = {
-    config_path = "~/.kube/config"
-  }
-}
-
-resource "helm_release" "argocd" {
-  name       = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argocd"
-
-  set = [
-    {
-      name  = "server.service.type"
-      value = "LoadBalancer"
-    }
-  ]
-}
+# resource "helm_release" "argocd" {
+#   name       = "argocd"
+#   repository = "https://argoproj.github.io/argo-helm"
+#   chart      = "argocd"
+#
+#   set = [
+#     {
+#       name  = "server.service.type"
+#       value = "LoadBalancer"
+#     }
+#   ]
+# }
 
 # resource "helm_release" "argocd" {
 #
@@ -52,4 +53,66 @@ resource "helm_release" "argocd" {
 # EOF
 #
 #   }
+# }
+#
+# resource "null_resource" "kube-config" {
+#
+#   triggers = {
+#     alltime = timestamp()
+#   }
+#
+#   provisioner "local-exec" {
+#     command = "aws eks update-kubeconfig --name ${var.env}"
+#   }
+#
+# }
+#
+# provider "helm" {
+#   kubernetes = {
+#     config_path = "~/.kube/config"
+#   }
+# }
+#
+# resource "helm_release" "argocd" {
+#
+#   depends_on = [null_resource.kube-config]
+#
+#   name       = "argocd"
+#   repository = "https://argoproj.github.io/argo-helm"
+#   chart      = "argo-cd"
+#
+#   set = [
+#     {
+#       name  = "server.service.type"
+#       value = "LoadBalancer"
+#     }
+#   ]
+# }
+#
+# resource "helm_release" "kube-stack" {
+#
+#   depends_on = [null_resource.kube-config]
+#
+#   name       = "kubestack"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "kube-prometheus-stack"
+#   set = [
+#     {
+#       name  = "prometheus.service.type"
+#       value = "LoadBalancer"
+#     }
+#   ]
+# }
+#
+# resource "helm_release" "file-beat" {
+#
+#   depends_on = [null_resource.kube-config]
+#
+#   name       = "filebeat"
+#   repository = "https://helm.elastic.co"
+#   chart      = "filebeat"
+#
+#   values = [
+#     file("${path.module}/filebeat.yml")
+#   ]
 # }

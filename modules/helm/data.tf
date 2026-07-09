44a158e0dc0085a87cf244b811b6395fd040291a
Manju9876 <manjunathb960650@gmail.com>
2026-07-09 11:31:51 +0530

    github actions practice

---
+++ modules/helm/data.tf
@@ -0,0 +1,23 @@
+data "kubernetes_service" "argocd" {
+
+  metadata {
+    name      = "argo-argocd-server"
+    namespace = "default"
+  }
+
+  depends_on = [
+    helm_release.argocd
+  ]
+}
+
+data "kubernetes_secret" "argocd_password" {
+
+  metadata {
+    name      = "argocd-initial-admin-secret"
+    namespace = "default"
+  }
+
+  depends_on = [
+    helm_release.argocd
+  ]
+}
--- modules/helm/main.tf
+++ modules/helm/main.tf
@@ -4,3 +4,33 @@ resource "null_resource" "kube_config" {
     command = "aws eks update-kubeconfig --name wmp-${var.env}"
   }
 }
+
+resource "helm_release" "argocd" {
+
+  name       = "argo"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "default"
+
+  values = [
+    file("${path.module}/argo.yml")
+  ]
+}
+
+# resource "null_resource" "argocd_login" {
+#
+#   depends_on = [
+#     helm_release.argocd
+#   ]
+#
+#   provisioner "local-exec" {
+#
+#     command = <<EOF
+# argocd login ${data.kubernetes_service.argocd.status[0].load_balancer[0].ingress[0].hostname} \
+#   --username admin \
+#   --password $(kubectl get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d) \
+#   --insecure
+# EOF
+#
+#   }
+# }

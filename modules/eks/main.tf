eb9cabc11dfeea1c4c06dddeed59c61d594ac488
Manju9876 <manjunathb960650@gmail.com>
2026-07-22 14:32:28 +0530

    github actions practice

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -72,12 +72,12 @@ resource "aws_eks_node_group" "main" {
   node_group_name = "node-group-1"
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = var.subnets
-  instance_types = ["t3.small"]
+  instance_types = ["t3.medium"]
 
   scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
   depends_on = [

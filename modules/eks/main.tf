23f0df66562c21fbce7f2581a9bf22f5bc412374
Manju9876 <manjunathb960650@gmail.com>
2026-07-22 20:58:10 +0530

    github actions  Prctice

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -75,9 +75,9 @@ resource "aws_eks_node_group" "main" {
   instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
+    desired_size = 4
+    max_size     = 4
+    min_size     = 4
   }
 
   depends_on = [

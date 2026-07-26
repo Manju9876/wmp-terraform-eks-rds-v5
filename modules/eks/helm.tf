3a0d14431ca28be5c6948d0abe594fe8780b1961
Manju9876 <manjunathb960650@gmail.com>
2026-07-26 11:56:21 +0530

    github actions  Prctice

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -11,28 +11,28 @@ resource "null_resource" "kube-config" {
   }
 }
 
-# provider "helm" {
-#   kubernetes = {
-#     config_path = "~/.kube/config"
-#   }
-# }
-#
-# resource "helm_release" "argocd" {
-#   name             = "argocd"
-#   repository       = "https://argoproj.github.io/argo-helm"
-#   chart            = "argo-cd"
-#   namespace        = "argocd"
-#   create_namespace = true
-#
-#   set {
-#     name  = "server.service.type"
-#     value = "LoadBalancer"
-#   }
-#
-#   depends_on = [
-#     null_resource.kube-config
-#   ]
-# }
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  depends_on = [
+    null_resource.kube-config
+  ]
+}
 
 #
 # resource "helm_release" "kube_prometheus_stack" {
